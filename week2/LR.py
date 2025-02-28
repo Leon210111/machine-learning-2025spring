877fd8fa2c9ab8179dfd068bd405a1ba1812e947
Zhuoyuan Li <ZhuoyuanLi@LeonBook>
2025-02-28 22:48:21 +0800

    week2 lab and report finished

--- week2/LR.py
+++ week2/LR.py
@@ -1,4 +1,8 @@
+from sklearn.linear_model import LogisticRegression
 from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
 wine=datasets.load_wine()
 X=wine.data
 Y=wine.target
@@ -8,16 +12,8 @@ from sklearn.preprocessing import StandardScaler
 scalar=StandardScaler()
 X_train_std=scalar.fit_transform(X_train)
 X_test_std=scalar.transform(X_test)
-from sklearn.decomposition import PCA
-pca=PCA(n_components=2)
-X_train_reduced=pca.fit_transform(X_train_std)
-X_test_reduced=pca.transform(X_test_std)
-#print("降维前：",X_test_std[:3],sep='/n')
-#print("降维后：",X_test_PCA[:3],sep='/n')
-from sklearn.linear_model import LogisticRegression
 lr=LogisticRegression()
-lr.fit(X_train_reduced,Y_train)
-Y_pred=lr.predict(X_test_reduced)
-from sklearn.metrics import accuracy_score
+lr.fit(X_train_std,Y_train)
+Y_pred=lr.predict(X_test_std)
 accuracy=accuracy_score(Y_test,Y_pred)
 print("Accuracy: ",accuracy)
